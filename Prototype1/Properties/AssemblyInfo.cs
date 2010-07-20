e6d1d4c258914a0afde61a4ceab387855ab7e8f5
Smooth <martinbarth@gmx.at>
2010-07-20 17:08:29 +0200

    Test 2, commit directories with 'git commit -a'

---
+++ Prototype1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Prototype1")]
+[assembly: AssemblyProduct("Prototype1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("MÄD Pseichos")]
+
+[assembly: AssemblyCopyright("Copyright © MÄD Pseichos 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c9424fb-13cb-4159-abc5-44937489a22d")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
